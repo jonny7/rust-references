b3938318c1cbb35681fc1f51c1d99aa81966e6e2
Jonathon Cooper <jonatho.cooper7@gmail.com>
2017-11-27 17:17:44 -0400

    reference doesn't work as immutable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+/*fn main() {
     let s1 = String::from("Hello");
 
     let len = calculate_length(&s1);
@@ -10,3 +10,13 @@ fn calculate_length(s: &String) -> usize { // s is a reference to a String
     s.len()
 } // Here, s goes out of scope. But because it does not have ownership of what
 // it refers to, nothing happens.
+*/
+
+fn main() {
+    let s = String::from("Hello");
+    change(&s);
+}
+
+fn change(some_string: &String) {
+    some_string.push_str(", World!");
+}
