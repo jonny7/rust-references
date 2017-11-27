72a87e2425a865dafd9c7cdb507a3fd063ecd07f
Jonathon Cooper <jonatho.cooper7@gmail.com>
2017-11-27 17:21:30 -0400

    multiple mutable references using scope

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,16 @@ fn calculate_length(s: &String) -> usize { // s is a reference to a String
 fn main() {
     let mut s = String::from("Hello");
     change(&mut s);
+
+    // multiple mutable references, uses scope to achieve this
+    let mut t = String::from("hello");
+
+    {
+        let r1 = &mut t;
+
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut t;
 }
 
 fn change(some_string: &mut String) {
